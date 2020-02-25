ac4b2226d80fa3a74c572c07a8ba7cacbe38da05
ashwanth ka <ashwanthka@gmail.com>
2020-02-24 18:27:23 -0600

    flower is added

--- Lab3/Flower.java
+++ Lab3/Flower.java
@@ -1,10 +1,35 @@
+package application;
+import javafx.geometry.Point2D;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
-public class Flower {
+public class Flower implements Garden{
 
-	public void move(double delataX, double delataY) {
-		// TODO Auto-generated method stub
-		circle.setCenterY(circle.getcenter()+dx)
-		
-	}
+Circle circle;
+public Flower(Point2D a,Color YELLOW, boolean c) {
+circle = new Circle();
+circle.setCenterX(a.getX());
+circle.setCenterY(a.getY());
+circle.setRadius(30);
+
+circle.setStroke(Color.YELLOW);
+circle.setStrokeWidth(4);
+
+
+}
+
+public Circle getCircle() {
+return circle;
+}
+public void move(double dx, double dy) {
+circle.setCenterX(circle.getCenterX()+dx);
+circle.setCenterY(circle.getCenterY()+dy);
+
+}
+
+public boolean contains(Point2D point) {
+return (circle.contains(point));
+}
+
 
 }
